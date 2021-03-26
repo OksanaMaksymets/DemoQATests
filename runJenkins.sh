1af0b902aaf7ceb2e0e404f333af0870c3f62052
omaksymets <omaksymets@future-processing.com>
2021-03-27 01:18:48 +0200

    bash file for jenkins

--- runJenkins.sh
+++ runJenkins.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 dt=$(date '+%d/%m/%Y %H:%M:%S');
 
-echo "$dt"
-echo "$PWD"
+$ echo "$dt"
+$ echo "$PWD"
 
-echo "each PATH element:"
+$ echo "each PATH element:"
 IFS=':'
 read -ra ADDR <<< "$PATH"
 for i in "${ADDR[@]}"; #accessing each element of array
