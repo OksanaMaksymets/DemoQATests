873ebfae7691dc79e5cd410bf73207ed0282cfe4
omaksymets <omaksymets@future-processing.com>
2021-03-27 04:17:21 +0200

    add parameters

--- run.cmd
+++ run.cmd
@@ -1,12 +1,12 @@
 @echo off
-echo %DATE% > %cd%\output.txt
+echo %DATE% > %cd%\build\output.txt
 echo %m%
-echo %PATH% > %cd%\output.txt
-echo %cd% > %cd%\output.txt
+echo %PATH% >> %cd%\build\output.txt
+echo %cd% >> %cd%\build\output.txt
 
 set loop=0
 :loop
-echo %Text%
+echo %Text% >> %cd%\build\parameters.txt
 set /a loop=%loop%+1
 if "%loop%"=="%Count%" goto next
 call :loop
