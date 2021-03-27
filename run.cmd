64eb0be90daf02ee42c64571f14d07aab60474f0
omaksymets <omaksymets@future-processing.com>
2021-03-27 04:31:39 +0200

    Build -> build

--- run.cmd
+++ run.cmd
@@ -1,14 +1,13 @@
 @echo off
 
-del %cd%\Build\parameters.txt
+del %cd%\parameters.txt
 
-echo %DATE% > %cd%\Build\output.txt
-echo %PATH% >> %cd%\Build\output.txt
-echo %cd% >> %cd%\Build\output.txt
+echo %DATE% > %cd%\output.txt
+echo %PATH% >> %cd%\output.txt
 
 set loop=0
 :loop
-echo %Text% >> %cd%\Build\parameters.txt
+echo %Text% >> %cd%\parameters.txt
 set /a loop=%loop%+1
 if "%loop%"=="%Count%" goto next
 call :loop
