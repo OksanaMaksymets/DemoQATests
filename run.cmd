1f474b0bfac838d489710140accd02a7f64ae8af
omaksymets <omaksymets@future-processing.com>
2021-03-27 04:38:03 +0200

    try to push txt to file

--- run.cmd
+++ run.cmd
@@ -3,11 +3,11 @@
 del %cd%\Build\parameters.txt 2>nul
 
 echo %DATE% > %cd%\Build\output.txt
-echo %PATH% >> %cd%\output.txt
+echo %PATH% >> %cd%\Build\output.txt
 
 set loop=0
 :loop
-echo %Text% >> %cd%\Build\parameters.txt
+echo %Text% >> %cd%\Build\parameters.txt 2>nul
 set /a loop=%loop%+1
 if "%loop%"=="%Count%" goto next
 call :loop
